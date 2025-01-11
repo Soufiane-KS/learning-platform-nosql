f8f127a6b495a37c088bae76d23e5e84997c3288
Soufiane <Soufianekousta1@gmail.com>
2025-01-11 19:28:25 +0100

    Implémentation d'une fonction générique de cache

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,19 @@
 // Question : Comment gérer efficacement le cache avec Redis ?
-// Réponse :
+// Réponse : Utiliser des durées d'expiration (TTL) pour éviter un cache obsolète, invalider les données lors de mises à jour critiques, et structurer les données pour un accès rapide.
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
-// Réponse :
+// Réponse : Adopter une convention de nommage claire, utiliser des clés courtes pour optimiser la mémoire, et éviter les espaces ou caractères spéciaux.
+
+const { redisClient } = require("../config/db");
 
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+  try {
+    await redisClient.set(key, JSON.stringify(data), 'EX',ttl)
+  } catch (error) {
+    console.error(error)
   }
+}
   
   module.exports = {
-    // TODO: Exporter les fonctions utilitaires
+    cacheData
   };
